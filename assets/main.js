aa8f8477fc144943f829d5abeb38a8604e0b20a2
Adriano de Camargo <adrianodecamargo123@gmail.com>
2024-02-13 20:15:01 -0300

    Ajustando exibição

--- assets/main.js
+++ assets/main.js
@@ -3,27 +3,31 @@ function calcular(){
     let aporteAmount = parseFloat(document.getElementById('aporte_amount').value)
     let interestRate = parseFloat(document.getElementById('interest_rate').value)
     let time = parseFloat(document.getElementById('time').value)
-    let resultadoField = document.getElementById('resultado')
-    let totalInvestidoField = document.getElementById('totalInvestido');
-    let totalJurosField = document.getElementById('totalJuros');
-    let percentualGanhoField = document.getElementById('percentualGanho');
     let resultado = initialAmount;
-    let resultField = document.getElementsByClassName('result')[0];
+    let contentResult = document.getElementsByClassName('result')[0]
 
- for(i=1;i<=time;i++){
-    resultado += resultado * (interestRate/100)
-    resultado += aporteAmount
+    if ((isNaN(initialAmount) || initialAmount == 0) || (isNaN(aporteAmount) || aporteAmount == 0) || (isNaN(interestRate) || interestRate == 0) || (isNaN(time) || time == 0)){
+
+    } else {
+
+     for(i=1;i<=time;i++){
+        resultado += resultado * (interestRate/100)
+        resultado += aporteAmount
+    }
+
+    resultado = resultado.toFixed(2)
+    let totalInvestido = (initialAmount + (aporteAmount*time)).toFixed(2)
+    let totalJuros = (resultado - totalInvestido).toFixed(2)
+    let percentualGanho = ((totalInvestido*100)/resultado).toFixed(2) 
+
+    contentResult.innerHTML = `
+    <h2>Aqui está o resultado:</h1>
+    <div class="result__content">
+    <span>O total investido foi de <span class="destaque" >R$ ${totalInvestido}</span>.</span>
+    <span>O ganho de juros compostos foi de <span class="destaque" id="totalJuros">R$ ${totalJuros}</span>.</span>
+    <span>O valor final acumulado foi de <span class="destaque" id="resultado">R$ ${resultado}</span>, um rendimento de <span class="destaque">${percentualGanho}%</span> do valor aplicado.</span>
+    </div>
+    `    
+    }
 }
-
-resultado = resultado.toFixed(2)
-let totalInvestido = (initialAmount + (aporteAmount*time)).toFixed(2)
-let totalJuros = (resultado - totalInvestido).toFixed(2)
-let percentualGanho = ((totalInvestido*100)/resultado).toFixed(2)
-
-totalInvestidoField.innerText = totalInvestido
-totalJurosField.innerText = totalJuros
-resultadoField.innerText = resultado
-percentualGanhoField.innerText = percentualGanho + '%'
-resultField.style = "visibility: visible" 
- 
-}
+    
